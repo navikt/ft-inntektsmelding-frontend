09036b37c7483233460d97c7b5ff12fc4f505de4
Johannes Moskvil <johannbm@users.noreply.github.com>
2024-04-25 14:39:36 +0200

    Legg til dekoratøren rundt appen (#3)
    Co-authored-by: Kristofer Giltvedt Selbekk <kristofer.selbekk@bekk.no>

---
+++ app/src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import type { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
+import React from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === "production"
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import("@tanstack/router-devtools").then((response) => ({
+          default: response.TanStackRouterDevtools,
+        })),
+      );
+
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
+  component: () => {
+    return (
+      <>
+        <React.Suspense fallback="">
+          <TanStackRouterDevtools position="bottom-right" />
+        </React.Suspense>
+        <Outlet />
+      </>
+    );
+  },
+});
