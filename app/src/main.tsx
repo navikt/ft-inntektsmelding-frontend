be35dd88dcdecb00779b2fd5477eada9943be726
Johannes Moskvil <johannbm@users.noreply.github.com>
2024-04-25 14:56:46 +0200

    Setter opp det som trengs for å bruke aksel+tailwind (#5)

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,4 +1,5 @@
 import "@navikt/ds-css";
+import "./index.css";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
