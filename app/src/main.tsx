2a5e4e3bb11e19259d40c708d32f5b14c98e878d
johannbm <johannes.moskvil@gmail.com>
2024-04-25 11:47:13 +0200

    Sett opp router

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,36 @@
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {createRouter, RouterProvider} from "@tanstack/react-router";
 import React from "react";
 import { createRoot } from "react-dom/client";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+import {routeTree} from "./routeTree.gen.ts";
+
+export const queryClient = new QueryClient();
+
+// TODO not found route
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: "intent",
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+});
+
+// Register things for typesafety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
 
 createRoot(document.querySelector("#root")!).render(
-  <React.StrictMode>Hei</React.StrictMode>,
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools buttonPosition="bottom-left" initialIsOpen={false} />
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>,
 );
