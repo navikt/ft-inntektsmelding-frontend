f81485477c640e018cd7452fecf37fcd47ad8135
johannbm <johannes.moskvil@gmail.com>
2024-06-04 13:39:24 +0200

    Revert "Legg til endepunkt i bff for å hente navn i token"
    This reverts commit 156068e6e5b12fd1dcc45f8eb1900946ab46a812.

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,6 @@ import { setupActuators } from "./actuators.js";
 import { setupApiProxy } from "./apiProxy.js";
 import { errorHandling } from "./errorHandler.js";
 import { setupStaticRoutes } from "./frontendRoute.js";
-import { internalRoutes } from "./internalRoutes.js";
 import { verifyToken } from "./tokenValidation.js";
 
 const app = express();
@@ -18,7 +17,6 @@ app.set("trust proxy", 1);
 
 app.use(verifyToken);
 
-internalRoutes(app);
 setupApiProxy(app);
 
 // Catch all route, må være sist
