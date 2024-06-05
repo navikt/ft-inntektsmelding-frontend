ad1d5a5f4b7f1f2ab6ef529ed7a000e6f37ef511
johannbm <johannes.moskvil@gmail.com>
2024-06-05 18:22:42 +0200

    try update urls

--- app/src/api/mutations.ts
+++ app/src/api/mutations.ts
@@ -1,20 +1,17 @@
 import type { SendInntektsmeldingRequestDto } from "~/types/api-models.ts";
 
-const FT_INNTEKTSMELDING_BACKEND_URL = `/server/api/imdialog`;
+const SERVER_URL = "fp-im-dialog/server/api";
 
 export async function sendInntektsmelding(
   sendInntektsmeldingRequest: SendInntektsmeldingRequestDto,
 ) {
-  const response = await fetch(
-    `${FT_INNTEKTSMELDING_BACKEND_URL}/send-inntektsmelding`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(sendInntektsmeldingRequest),
+  const response = await fetch(`${SERVER_URL}/send-inntektsmelding`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
     },
-  );
+    body: JSON.stringify(sendInntektsmeldingRequest),
+  });
 
   return (await response.json()) as unknown;
 }
