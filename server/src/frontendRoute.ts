e79aa051391c08fd8377f31a3b46d3bd9b805904
johannbm <johannes.moskvil@gmail.com>
2024-06-11 12:47:45 +0200

    Modify vite-mode

--- server/src/frontendRoute.ts
+++ server/src/frontendRoute.ts
@@ -14,8 +14,14 @@ const csp =
     ? await buildCspHeader({}, { env: config.app.env })
     : await buildCspHeader(
         {
-          "script-src-elem": ["http://localhost:5173"],
-          "connect-src": ["ws://localhost:5173"],
+          "script-src-elem": [
+            "http://localhost:5173/fp-im-dialog",
+            "http://localhost:5173/k9-im-dialog",
+          ],
+          "connect-src": [
+            "ws://localhost:5173/fp-im-dialog",
+            "ws://localhost:5173/k9-im-dialog",
+          ],
         },
         { env: config.app.env },
       );
@@ -44,20 +50,24 @@ function addLocalViteServerHandlerWithDecorator(router: Router) {
   router.use(cookieParser());
   router.get("/vite-on", (request, response) => {
     setViteCookie(response, true);
-    return response.redirect("/");
+    return response.redirect(`/${config.app.nestedPath}`);
   });
   router.get("/vite-off", (request, response) => {
     setViteCookie(response, false);
-    return response.redirect("/");
+    return response.redirect(`/${config.app.nestedPath}`);
   });
   router.get("*", async (request, response, next) => {
     const localViteServerIsEnabled =
       request.cookies["use-local-vite-server"] === "true";
     if (localViteServerIsEnabled) {
       const html = await injectDecorator(viteDevelopmentServerPath);
+      const modifiedHtml = html.replaceAll(
+        "http://localhost:5173",
+        `http://localhost:5173/${config.app.nestedPath}`,
+      );
       response.setHeader("Content-Security-Policy", csp);
 
-      return response.send(html);
+      return response.send(modifiedHtml);
     }
     return next();
   });
