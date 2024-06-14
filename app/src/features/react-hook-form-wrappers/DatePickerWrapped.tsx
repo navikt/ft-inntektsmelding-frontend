09093d987c11834d7df34cc2eb3a43901641596c
johannbm <johannes.moskvil@gmail.com>
2024-06-14 13:18:46 +0200

    ignorer any

--- app/src/features/react-hook-form-wrappers/DatePickerWrapped.tsx
+++ app/src/features/react-hook-form-wrappers/DatePickerWrapped.tsx
@@ -2,6 +2,7 @@ import type { DateInputProps } from "@navikt/ds-react";
 import { DatePicker, useDatepicker } from "@navikt/ds-react";
 import { useController } from "react-hook-form";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- usikker på hvordan rules skal types, og om det er såå viktig.
 type DatePickerWrappedProps = { name: string; rules?: any } & DateInputProps;
 
 export function DatePickerWrapped({
