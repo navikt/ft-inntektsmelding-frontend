0a616b9bc6b54aa78690dd1699dfa79fa7951a2c
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-06-18 13:56:01 +0200

    Fjern unødig komponent

--- app/src/routes/$id.index.tsx
+++ app/src/routes/$id.index.tsx
@@ -1,7 +1,6 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$id/")({
-  component: () => null,
   loader: () => {
     throw redirect({ from: "/$id", to: "/$id/dine-opplysninger" });
   },
