412e31735dd5072c46e9921c20c2bd9d2ac4aa23
Johannes Moskvil <johannbm@users.noreply.github.com>
2024-09-03 12:48:42 +0200

    Legg til enkel notfound route (#53)

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,7 @@
 import "@navikt/ds-css";
 import "./index.css";
 
+import { BodyShort, Box, Button, Heading, VStack } from "@navikt/ds-react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
@@ -11,8 +12,8 @@ import { routeTree } from "./routeTree.gen.ts";
 
 export const queryClient = new QueryClient();
 
-// TODO not found route
 const router = createRouter({
+  defaultNotFoundComponent: NotFoundComponent,
   routeTree,
   basepath: import.meta.env.BASE_URL,
   context: {
@@ -40,3 +41,24 @@ createRoot(document.querySelector("#root")!).render(
     </QueryClientProvider>
   </React.StrictMode>,
 );
+
+function NotFoundComponent() {
+  return (
+    <Box data-aksel-template="404-v2" paddingBlock="20 16">
+      <VStack align="start" gap="12">
+        <div>
+          <Heading level="1" size="large" spacing>
+            Beklager, vi fant ikke siden
+          </Heading>
+          <BodyShort>
+            Denne siden kan være slettet eller flyttet, eller det er en feil i
+            lenken.
+          </BodyShort>
+        </div>
+        <Button as="a" href="https://arbeidsgiver.nav.no/min-side-arbeidsgiver">
+          Gå til Min side arbeidsgiver
+        </Button>
+      </VStack>
+    </Box>
+  );
+}
