d7c15a96d781f49dd67d1adc632387f291073e31
Kristofer Giltvedt Selbekk <selbekk@users.noreply.github.com>
2024-09-11 14:01:52 +0200

    Legg til en error boundary for generelle tekniske feil (#62)

--- app/src/routes/__root.tsx
+++ app/src/routes/__root.tsx
@@ -7,6 +7,7 @@ import {
 import React from "react";
 
 import { hentGrunnbeløpOptions } from "~/api/queries.ts";
+import { GenerellFeilside } from "~/features/error-boundary/GenerellFeilside";
 import { VisHjelpeteksterStateProvider } from "~/features/Hjelpetekst.tsx";
 
 const TanStackRouterDevtools = import.meta.env.PROD
@@ -25,6 +26,7 @@ export const Route = createRootRouteWithContext<{
     // Bruker prefetch for å ikke vente på dette nettverkskallet, men gjøre det klar i cache så fort som mulig
     context.queryClient.prefetchQuery(hentGrunnbeløpOptions());
   },
+  errorComponent: GenerellFeilside,
 
   component: () => {
     return (
