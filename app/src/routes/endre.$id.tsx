fcf6c30be811583c8145bc0c3ad1eed011499d53
Kristofer Giltvedt Selbekk <kristofer.giltvedt.selbekk@nav.no>
2024-10-09 10:11:07 +0200

    Fjern endre-inntektsmelding view
    Denne routen var aldri i bruk, og man endrer gjennom den mer vanlige flyten

--- app/src/routes/endre.$id.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { EndreInntektsmelding } from "~/views/endre-inntektsmelding/EndreInntektsmelding";
-
-export const Route = createFileRoute("/endre/$id")({
-  component: EndreInntektsmelding,
-});
