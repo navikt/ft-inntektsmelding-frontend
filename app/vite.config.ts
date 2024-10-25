ca36542dbee04678ae4d426384634bf876d9648b
johannbm <johannes.moskvil@gmail.com>
2024-10-25 14:13:53 +0200

    fjern gammelt forsøk

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -12,12 +12,6 @@ export default defineConfig({
       "~": fileURLToPath(new URL("src", import.meta.url)),
     },
   },
-  // build: {
-  //   manifest: true,
-  //   rollupOptions: {
-  //     external: ["./nais.js"],
-  //   },
-  // },
   server: {
     origin: "http://localhost:5173",
   },
