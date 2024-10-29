51ef6052b6fd8d8af3d5e6b2c9f64dd2c17e3442
johannbm <johannes.moskvil@gmail.com>
2024-10-29 13:53:11 +0100

    Lag test for utgått oppgave side

--- app/tests/mocks/grunnlag.ts
+++ app/tests/mocks/grunnlag.ts
@@ -1,3 +1,5 @@
+import { OpplysningerDto } from "~/types/api-models.ts";
+
 export const enkeltGrunnlagResponse = {
   person: {
     fornavn: "UNDERFUNDIG",
@@ -33,5 +35,41 @@ export const enkeltGrunnlagResponse = {
   ],
   startdatoPermisjon: "2024-05-30",
   ytelse: "FORELDREPENGER" as const,
-  forespørselUuid: "1",
-};
+} satisfies OpplysningerDto;
+
+export const utgåttOpplysningerResponse = {
+  person: {
+    fornavn: "UNDERFUNDIG",
+    etternavn: "DYREFLOKK",
+    fødselsnummer: "27527827812",
+    aktørId: "2715347149890",
+  },
+  arbeidsgiver: {
+    organisasjonNavn: "Papir- og pappvareproduksjon el.",
+    organisasjonNummer: "810007842",
+  },
+  innsender: {
+    fornavn: "BERØMT",
+    etternavn: "FLYTTELASS",
+  },
+  forespørselStatus: "UTGÅTT" as const,
+  inntekter: [
+    {
+      fom: "2024-02-01",
+      tom: "2024-02-29",
+      arbeidsgiverIdent: "810007842",
+    },
+    {
+      fom: "2024-03-01",
+      tom: "2024-03-31",
+      arbeidsgiverIdent: "810007842",
+    },
+    {
+      fom: "2024-04-01",
+      tom: "2024-04-30",
+      arbeidsgiverIdent: "810007842",
+    },
+  ],
+  startdatoPermisjon: "2024-05-30",
+  ytelse: "FORELDREPENGER" as const,
+} satisfies OpplysningerDto;
