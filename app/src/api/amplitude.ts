1420e4e045b8ccd14a734a9134233ada74aba994
johannbm <johannes.moskvil@gmail.com>
2024-11-12 13:05:01 +0100

    legg til taksonomi

--- app/src/api/amplitude.ts
+++ app/src/api/amplitude.ts
@@ -1,8 +1,15 @@
+//Bruk kun navn fra taksonomien. Med utgangspunkt i https://github.com/navikt/analytics-taxonomy
+type EventNamesTaksonomi =
+  | "readmore lukket"
+  | "readmore åpnet"
+  | "switch åpnet"
+  | "switch lukket";
+
 export const loggAmplitudeEvent = ({
   eventName,
   eventData,
 }: {
-  eventName: string;
+  eventName: EventNamesTaksonomi;
   eventData?: Record<string, string>;
 }) => {
   // eslint-disable-next-line unicorn/prefer-global-this -- klarer ikke få TS til å bli riktig for globalThis
