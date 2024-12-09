0bbf731ea14a123e01e9ea0be42d79fbaf1d1b06
johannbm <johannes.moskvil@gmail.com>
2024-12-09 11:04:09 +0100

    legg til avbryt knapp

--- app/src/features/inntektsmelding/InntektsmeldingRootLayout.tsx
+++ app/src/features/inntektsmelding/InntektsmeldingRootLayout.tsx
@@ -1,3 +1,5 @@
+import { XMarkIcon } from "@navikt/aksel-icons";
+import { Button, HStack } from "@navikt/ds-react";
 import { setBreadcrumbs } from "@navikt/nav-dekoratoren-moduler";
 import { getRouteApi, Outlet, useLocation } from "@tanstack/react-router";
 import { useEffect } from "react";
@@ -28,12 +30,12 @@ export const InntektsmeldingRootLayout = () => {
     ]);
   }, [id]);
 
+  const erPåKvitteringssiden = location.pathname.includes("kvittering");
+
   return (
     <InntektsmeldingSkjemaStateProvider>
       <RotLayout
-        background={
-          location.pathname.includes("kvittering") ? "bg-default" : "bg-subtle"
-        }
+        background={erPåKvitteringssiden ? "bg-default" : "bg-subtle"}
         tittel={`Inntektsmelding ${formatYtelsesnavn(opplysninger.ytelse)}`}
         undertittel={
           <div className="flex gap-3">
@@ -46,6 +48,18 @@ export const InntektsmeldingRootLayout = () => {
         }
       >
         <Outlet />
+        {!erPåKvitteringssiden && (
+          <HStack align="center" justify="center">
+            <Button
+              as="a"
+              href="/min-side-arbeidsgiver"
+              icon={<XMarkIcon />}
+              variant="tertiary"
+            >
+              Avbryt
+            </Button>
+          </HStack>
+        )}
       </RotLayout>
     </InntektsmeldingSkjemaStateProvider>
   );
