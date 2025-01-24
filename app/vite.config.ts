c139b08612ad71a0ad7c80ec65ad3c1d100d0157
johannbm <johannes.moskvil@gmail.com>
2025-01-24 10:47:39 +0100

    oppdater configs

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,12 +1,13 @@
 import { fileURLToPath, URL } from "node:url";
 
+import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [tailwindcss(), react(), TanStackRouterVite()],
   resolve: {
     alias: {
       "~": fileURLToPath(new URL("src", import.meta.url)),
