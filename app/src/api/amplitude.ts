044a95c0fa53923e9dee8b987a1682dff2145b57
johannbm <johannes.moskvil@gmail.com>
2025-01-30 14:00:52 +0100

    dekoratøren finnes ikke hvis cookies er rejected

--- app/src/api/amplitude.ts
+++ app/src/api/amplitude.ts
@@ -16,7 +16,7 @@ export const loggAmplitudeEvent = ({
 }) => {
   if (!isDev) {
     // eslint-disable-next-line unicorn/prefer-global-this -- klarer ikke få TS til å bli riktig for globalThis
-    window.dekoratorenAmplitude({
+    window.dekoratorenAmplitude?.({
       origin: "ft-inntektsmelding-frontend",
       eventName,
       eventData,
