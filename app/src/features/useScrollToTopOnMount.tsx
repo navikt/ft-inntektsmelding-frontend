2692bcb5418ad47b6fcb75e2b932dfcce0fff468
vebnor <vebjorn.nordby@nav.no>
2025-09-01 12:23:16 +0200

    smooth scroll

--- app/src/features/useScrollToTopOnMount.tsx
+++ app/src/features/useScrollToTopOnMount.tsx
@@ -2,6 +2,6 @@ import { useEffect } from "react";
 
 export const useScrollToTopOnMount = () => {
   useEffect(() => {
-    window.scrollTo(0, 0);
+    window.scrollTo({ top: 0, behavior: "smooth" });
   }, []);
 };
